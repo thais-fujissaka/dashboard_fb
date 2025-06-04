422503b097b582876f56eab0cb5eb6cb1b961069
thais-fujissaka <thais.fujissaka@gmail.com>
2025-06-04 17:54:47 -0300

    em progresso: redirecionamento inicial gazit

--- Login.py
+++ Login.py
@@ -48,7 +48,7 @@ def main():
                 )
     else:
         st.write("Você está logado!")
-        st.markdown("Redirecionando para a página Home...")
+        st.markdown("Redirecionando...")
         st.switch_page("pages/1_Calendário_de_Eventos.py")
 
 if __name__ == "__main__":
--- utils/user.py
+++ utils/user.py
@@ -6,6 +6,7 @@ def login(userName: str, userPassword: str) -> bool:
         return False
     
     users = list(st.secrets["gazit_users"].keys())
+    # Login via API (usuário não Gazit)
     if userName not in users:
       login_data = {
         "username": userName,
@@ -16,9 +17,11 @@ def login(userName: str, userPassword: str) -> bool:
       if "error" in response:
         return False
       elif response.get("data", {}).get("success") == True:
+        print(login_data)
         return login_data
       else:
           return False
+    # Login usuário Gazit 
     else:
       senha_correta = st.secrets["gazit_users"][userName]
       if userPassword == senha_correta:
@@ -26,10 +29,12 @@ def login(userName: str, userPassword: str) -> bool:
           "data": {
             "success": True,
             "user": {
-              "name": userName
+              "name": userName,
+              "permission": "Gazit"
             }
           }
         }
+        print(user_data)
         return user_data
       else:
         return False
