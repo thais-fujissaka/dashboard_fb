8ccf0c9a99fab7510ebfafdda667274fece592a1
thais-fujissaka <thais.fujissaka@gmail.com>
2025-06-17 15:03:30 -0300

    feat: filtro por tipo de data competencia, recebimento lead ou envio proposta

--- utils/functions/kpis_conversao_eventos_priceless.py
+++ utils/functions/kpis_conversao_eventos_priceless.py
@@ -101,36 +101,38 @@ def cards_valor_propostas(valor_leads_recebidos, valor_lancadas, valor_confirmad
     kpi_card_propostas_valores(valor_em_negociacao, amarelo, "Valor de propostas em negociação:")
     
 
-def calculo_numero_propostas(df_eventos, df_eventos_data_lead, ano, mes):
+def calculo_numero_propostas(df_eventos, ano, mes):
     """
     Calcula o número de propostas lançadas, confirmadas, declinadas e em negociação
     para o ano e mês especificados.
     """
     df_eventos['Data do Evento'] = pd.to_datetime(df_eventos['Data do Evento'])
+    df_eventos_com_proposta_enviada = df_eventos[df_eventos['Data Envio Proposta'].notnull()]
     
-    num_leads_recebidos = len(df_eventos_data_lead)
-    num_lancadas = len(df_eventos)
-    num_confirmadas = len(df_eventos[df_eventos['Status do Evento'] == 'Confirmado'])
-    num_declinadas = len(df_eventos[df_eventos['Status do Evento'] == 'Declinado'])
-    num_em_negociacao = len(df_eventos[df_eventos['Status do Evento'] == 'Em negociação'])
+    num_leads_recebidos = len(df_eventos)
+    num_enviadas = len(df_eventos_com_proposta_enviada)
+    num_confirmadas = len(df_eventos_com_proposta_enviada[df_eventos_com_proposta_enviada['Status do Evento'] == 'Confirmado'])
+    num_declinadas = len(df_eventos_com_proposta_enviada[df_eventos_com_proposta_enviada['Status do Evento'] == 'Declinado'])
+    num_em_negociacao = len(df_eventos_com_proposta_enviada[df_eventos_com_proposta_enviada['Status do Evento'] == 'Em negociação'])
     
-    return num_leads_recebidos, num_lancadas, num_confirmadas, num_declinadas, num_em_negociacao
+    return num_leads_recebidos, num_enviadas, num_confirmadas, num_declinadas, num_em_negociacao
 
 
-def calculo_valores_propostas(df_eventos, df_eventos_data_lead, ano, mes):
+def calculo_valores_propostas(df_eventos, ano, mes):
     """
     Calcula os valores das propostas lançadas, confirmadas, declinadas e em negociação
     para o ano e mês especificados.
     """
     df_eventos['Data do Evento'] = pd.to_datetime(df_eventos['Data do Evento'])
+    df_eventos_com_proposta_enviada = df_eventos[df_eventos['Data Envio Proposta'].notnull()]
     
-    valor_leads_recebidos = df_eventos_data_lead['Valor Total'].sum() if not df_eventos_data_lead.empty else 0
-    valor_lancadas = df_eventos['Valor Total'].sum()
-    valor_confirmadas = df_eventos[df_eventos['Status do Evento'] == 'Confirmado']['Valor Total'].sum()
-    valor_declinadas = df_eventos[df_eventos['Status do Evento'] == 'Declinado']['Valor Total'].sum()
-    valor_em_negociacao = df_eventos[df_eventos['Status do Evento'] == 'Em negociação']['Valor Total'].sum()
+    valor_leads_recebidos = df_eventos['Valor Total'].sum() if not df_eventos.empty else 0
+    valor_enviadas = df_eventos_com_proposta_enviada['Valor Total'].sum()
+    valor_confirmadas = df_eventos_com_proposta_enviada[df_eventos_com_proposta_enviada['Status do Evento'] == 'Confirmado']['Valor Total'].sum()
+    valor_declinadas = df_eventos_com_proposta_enviada[df_eventos_com_proposta_enviada['Status do Evento'] == 'Declinado']['Valor Total'].sum()
+    valor_em_negociacao = df_eventos_com_proposta_enviada[df_eventos_com_proposta_enviada['Status do Evento'] == 'Em negociação']['Valor Total'].sum()
     
-    return valor_leads_recebidos,valor_lancadas, valor_confirmadas, valor_declinadas, valor_em_negociacao
+    return valor_leads_recebidos,valor_enviadas, valor_confirmadas, valor_declinadas, valor_em_negociacao
 
 def grafico_pizza_num_propostas(num_confirmadas, num_declinadas, num_em_negociacao):
         
