9b41bb03a5bdc764113e1fdbea417074b97175a2
thais-fujissaka <thais.fujissaka@gmail.com>
2025-07-11 17:39:46 -0300

    feat: pagina de kpis de clientes de eventos

---
+++ pages/9_KPIs_Historico_Clientes_Eventos.py
@@ -0,0 +1,153 @@
+import streamlit as st
+from utils.components import *
+from utils.functions.date_functions import *
+from utils.functions.general_functions import *
+from utils.queries import *
+from utils.functions.parcelas import *
+from utils.user import *
+from utils.functions.kpis_clientes_eventos import *
+
+st.set_page_config(
+	page_icon=":busts_in_silhouette:",
+	page_title="KPI's de Ventas - Histórico e Recorrência de Clientes",
+	layout="wide",
+	initial_sidebar_state="collapsed"
+)
+
+if 'loggedIn' not in st.session_state or not st.session_state['loggedIn']:
+	st.switch_page('Login.py')
+
+def main():
+    st.markdown(" <style>iframe{ height: 500px !important } ", unsafe_allow_html=True)
+    config_sidebar()
+
+    df_clientes_eventos = GET_CLIENTES_EVENTOS()
+
+    # Força espaçamento e quebra no DOM
+    st.markdown("<div style='margin-top: 30px'></div>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns([6, 1, 1])
+    with col1:
+        st.title(":busts_in_silhouette: KPI's de Vendas - Histórico e Recorrência de Clientes")
+    with col2:
+        st.button(label='Atualizar', key='atualizar_historico_clientes_eventos', on_click=st.cache_data.clear)
+    with col3:
+        if st.button('Logout', key='logout_historico_clientes_eventos'):
+            logout()
+               
+    st.divider()
+
+    tab1, tab2 = st.tabs(["**Histórico de Clientes**", "**Recorrência de Clientes**"])
+
+    with tab1:
+        st.markdown("## Histórico de Clientes", help="Buscar informações de cadastro dos clientes e informações de todos os eventos que já foram feitos por aquele cliente em todas as unidades.")
+        st.divider()
+
+        df_clientes = df_clientes_eventos.copy().drop_duplicates(subset=['ID Cliente'])
+        df_clientes['ID - Cliente'] = df_clientes['ID Cliente'].astype(str) + ' - ' + df_clientes['Cliente']
+        lista_clientes = df_clientes['ID - Cliente'].tolist()
+        
+        clientes_selecionados = st.multiselect(
+            label="Buscar Cliente",
+            options=lista_clientes,
+            default=None,
+            key="casas_historico_clientes",
+            placeholder="Nome do Cliente",
+        )
+        st.divider()
+
+        if clientes_selecionados:
+            lista_ids_clientes_selecionados = [int(cliente.split(' - ')[0]) for cliente in clientes_selecionados]
+            df_clientes_selecionados = df_clientes[df_clientes['ID Cliente'].isin(lista_ids_clientes_selecionados)].copy()
+            df_clientes_selecionados.fillna('Não informado', inplace=True)
+
+            for _, cliente in df_clientes_selecionados.groupby('ID Cliente'):
+
+                st.markdown(f'### :material/group:  {cliente['Cliente'].values[0]}')
+                st.write('')
+
+                col1, col2, col3 = st.columns([1, 1, 1], gap="large", vertical_alignment="top")
+                with col1:
+                    st.markdown(f'**Documento:** {cliente['Documento'].values[0]}')
+                    st.markdown(f'**E-mail:** {cliente['Email'].values[0]}')
+                with col2:
+                    st.markdown(f'**Telefone:** {cliente['Telefone'].values[0]}')
+                    st.markdown(f'**Pessoa de Contato:** {cliente['Pessoa de Contato'].values[0]}')
+                with col3:
+                    st.markdown(f'**Endereço:** {cliente['Endereço'].values[0]}')
+                    st.markdown(f'**CEP:** {cliente['CEP'].values[0]}')
+
+                st.markdown("#### Eventos Realizados")
+                df_eventos_realizados_cliente = df_clientes_eventos.copy()
+                df_eventos_realizados_cliente = df_eventos_realizados_cliente[df_eventos_realizados_cliente['ID Cliente'] == cliente['ID Cliente'].values[0]]
+                df_eventos_realizados_cliente = df_format_date_columns_brazilian(df_eventos_realizados_cliente, ['Data Evento', 'Data Recebimento Lead', 'Data Envio Proposta', 'Data Contratação'])
+                df_eventos_realizados_cliente = format_columns_brazilian(df_eventos_realizados_cliente, ['Valor Total Evento', 'Valor AB', 'Valor Locação Aroo 1', 'Valor Locação Aroo 2', 'Valor Locação Aroo 3', 'Valor Locação Anexo', 'Valor Locação Notie', 'Valor Locação Mirante', 'Valor Imposto'])
+                st.dataframe(df_eventos_realizados_cliente.drop(columns=['ID Cliente', 'Cliente', 'Documento', 'Email', 'Telefone', 'Pessoa de Contato', 'Endereço', 'CEP']), use_container_width=True, hide_index=True)
+                st.divider()
+        else:
+            st.warning("Selecione um cliente para visualizar as informações.")
+            st.divider()
+
+    with tab2:
+        col1, col2 = st.columns([1, 1], vertical_alignment='center')
+        with col1:
+            st.markdown("## Recorrência de Clientes")
+        with col2:
+            periodo = input_periodo_datas(key='data_inicio')
+        st.divider()  
+        
+        
+        if periodo and len(periodo) == 2:
+            data_inicio = periodo[0]
+            data_fim = periodo[1]
+            df_numero_clientes_periodo = df_clientes_eventos.copy()
+            df_numero_clientes_periodo = df_numero_clientes_periodo[(df_numero_clientes_periodo['Data Evento'] >= data_inicio) & (df_numero_clientes_periodo['Data Evento'] <= data_fim)]
+            # num_eventos_periodo = len(df_numero_clientes_periodo)
+            # st.markdown(f'N° de eventos realizados no período:{num_eventos_periodo}')
+            # st.dataframe(df_numero_clientes_periodo, use_container_width=True, hide_index=True)
+            df_numero_clientes_periodo['N° Eventos'] = df_numero_clientes_periodo.groupby('ID Cliente')['ID Evento'].transform('count')
+            df_numero_clientes_periodo = df_numero_clientes_periodo.groupby('ID Cliente').agg({
+                'Cliente': 'first',
+                'Documento': 'first',
+                'Email': 'first',
+                'Telefone': 'first',
+                'Pessoa de Contato': 'first',
+                'Endereço': 'first',
+                'CEP': 'first',
+                'Data Evento': 'max',
+                'Valor Total Evento': 'sum',
+                'N° Eventos': 'first'
+            }).reset_index()
+            num_clientes_atendidos = len(df_numero_clientes_periodo)
+            df_numero_clientes_periodo = df_numero_clientes_periodo.rename(columns={
+                'Valor Total Evento': 'Valor Total Eventos',
+                'Data Evento': 'Data Último Evento',
+            })
+            df_numero_clientes_periodo_formatado = df_format_date_columns_brazilian(df_numero_clientes_periodo, ['Data Último Evento'])
+            df_numero_clientes_periodo_formatado = format_columns_brazilian(df_numero_clientes_periodo_formatado, ['Valor Total Eventos'])
+            df_numero_clientes_periodo = df_numero_clientes_periodo.sort_values(by='N° Eventos', ascending=True).reset_index(drop=True)
+
+            with st.container(border=True):
+                col1, col2, col3 = st.columns([0.1, 3, 0.1], gap="large", vertical_alignment="top")
+                with col2:
+                    st.markdown(f'### Clientes atendidos no período')
+                    st.dataframe(df_numero_clientes_periodo_formatado, use_container_width=True, hide_index=True)
+                    st.markdown(f'N° de Clientes Atendidos: {num_clientes_atendidos}')
+
+            with st.container(border=True):
+                col1, col2, col3 = st.columns([0.1, 3, 0.1], gap="large", vertical_alignment="top")
+                with col2:
+                    st.markdown("### Número de Eventos por Cliente")
+                    grafico_ranking_clientes_por_num_eventos(df_numero_clientes_periodo)
+
+            with st.container(border=True):
+                col1, col2, col3 = st.columns([0.1, 3, 0.1], gap="large", vertical_alignment="top")
+                with col2:
+                    st.markdown("### Valor de Eventos por Cliente")
+                    df_numero_clientes_periodo = df_numero_clientes_periodo.sort_values(by='Valor Total Eventos', ascending=True).reset_index(drop=True)
+                    grafico_ranking_clientes_por_valor_eventos(df_numero_clientes_periodo)
+
+        elif not periodo or len(periodo) != 2:
+            st.warning("Por favor, selecione um período válido.")
+
+if __name__ == '__main__':
+    main()
---
+++ utils/functions/kpis_clientes_eventos.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from streamlit_echarts import st_echarts
+from utils.functions.general_functions import *
+
+
+def grafico_ranking_clientes_por_num_eventos(df):
+    clientes_data = df['Cliente'].to_list()
+    num_eventos_data = df['N° Eventos'].to_list()
+    option = {
+        "tooltip": {
+            "trigger": "axis",
+            "axisPointer": {"type": "shadow"}
+        },
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "xAxis": {"type": "value"},
+        "yAxis": {
+            "type": "category",
+            "data": clientes_data
+        },
+        "series": [{
+            "name": "Nº Eventos",
+            "type": "bar",
+            "label": {"show": True, "position": "right"},
+            "data": num_eventos_data,
+            "itemStyle": {"color": "#FAC858"}
+        }]
+    }
+
+    st_echarts(options=option, height="500px")
+
+def grafico_ranking_clientes_por_valor_eventos(df):
+    # Cria series de dados para o gráfico
+    clientes = df['Cliente'].to_list()
+    valores = df['Valor Total Eventos'].astype(float).to_list()
+
+    # Labels formatados
+    labels = [format_brazilian(valor) for valor in valores]
+
+    # Dados com labels
+    valores_com_labels = [{"value": v, "label": {"show": True, "position": "right", "color": "#000", "formatter": lbl}} for v, lbl in zip(valores, labels)]
+    
+    option = {
+        "tooltip": {
+            "trigger": "axis",
+            "axisPointer": {"type": "shadow"}
+        },
+        "grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
+        "xAxis": {"type": "value"},
+        "yAxis": {
+            "type": "category",
+            "data": clientes
+        },
+        "series": [{
+            "name": "Nº Eventos",
+            "type": "bar",
+            "label": {"show": True, "position": "right"},
+            "data": valores_com_labels,
+            "itemStyle": {"color": "#FAC858"}
+        }]
+    }
+
+    st_echarts(options=option, height="500px")
