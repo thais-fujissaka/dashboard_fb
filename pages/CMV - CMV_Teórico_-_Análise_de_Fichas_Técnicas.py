34ded69ad97eebb90cc2d7e1a95f3e7dec3cbdae
thais-fujissaka <thais.fujissaka@gmail.com>
2025-10-14 18:12:46 -0300

    feat: adiciona cmv orcado na aba de cmv teorico

--- pages/CMV - CMV_Teórico_-_Análise_de_Fichas_Técnicas.py
+++ pages/CMV - CMV_Teórico_-_Análise_de_Fichas_Técnicas.py
@@ -211,9 +211,13 @@ def main():
     percentual_itens_cmv_regular = round(qtde_itens_cmv_regular / qtde_total_itens * 100, 2)
     percentual_itens_cmv_critico = round(qtde_itens_cmv_critico / qtde_total_itens * 100, 2)
 
+    df_cmv_orcado_AB = GET_CMV_ORCADO_AB()
+    cmv_orcado_AB = df_cmv_orcado_AB[(df_cmv_orcado_AB['Ano'] == ano) & (df_cmv_orcado_AB['Mês'] == mes) & (df_cmv_orcado_AB['ID Casa'] == id_casa)]['% CMV Orçado'].values[0]
+    cmv_orcado_AB = format_brazilian(cmv_orcado_AB)
+
     col1, col2, col3, col4 = st.columns([1, 1, 1, 1], vertical_alignment='center')
     with col1:
-        kpi_card_cmv_teorico('CMV Orçado', f'-', background_color="#FFFFFF", title_color="#333", value_color="#000")
+        kpi_card_cmv_teorico('CMV Orçado', f'{cmv_orcado_AB} %', background_color="#FFFFFF", title_color="#333", value_color="#000")
     with col2:
         kpi_card_cmv_teorico('Quantidade de Itens com CMV Unitário BOM', f'{qtde_itens_cmv_bom}', background_color="#FFFFFF", title_color="#333", value_color="#000", valor_percentual=percentual_itens_cmv_bom)
     with col3:
