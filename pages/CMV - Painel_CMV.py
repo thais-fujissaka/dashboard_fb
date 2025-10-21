a2d0cb87d53c647c6c75d6fcf5bda3f2d283e50c
thais-fujissaka <thais.fujissaka@gmail.com>
2025-10-21 15:49:43 -0300

    feat: grafico cmv orçado

--- pages/CMV - Painel_CMV.py
+++ pages/CMV - Painel_CMV.py
@@ -16,6 +16,84 @@ st.set_page_config(
 if 'loggedIn' not in st.session_state or not st.session_state['loggedIn']:
     st.switch_page('Login.py')
 
+from streamlit_echarts import st_echarts
+import streamlit as st
+
+def grafico_cmv_orcado(df_cmv_orcado):
+
+    dados_cmv_orcado_em_reais = df_cmv_orcado['Valor Orçamento CMV'].astype(float).tolist()
+    dados_cmv_orcado_porcentagem = df_cmv_orcado['% CMV Orçado'].astype(float).tolist()
+
+    dict_meses = {
+        1: 'Jan', 2: 'Fev', 3: 'Mar', 4: 'Abr', 5: 'Mai', 6: 'Jun',
+        7: 'Jul', 8: 'Ago', 9: 'Set', 10: 'Out', 11: 'Nov', 12: 'Dez'
+    }
+    meses = [dict_meses[mes] for mes in df_cmv_orcado['Mês']]
+
+    option = {
+        "title": {
+            "text": "CMV Orçado",
+            "left": "center",
+            "textStyle": {
+            "fontSize": 18,
+            "fontWeight": "bold",
+            "color": "#333"
+        }
+        },
+        "tooltip": {"trigger": "axis"},
+        "legend": {
+            "data": ["Valor CMV Orçado (R$)", "CMV Orçado (%)"],
+            "bottom": 0,
+            "left": "center"
+        },
+        "grid": {
+            "left": "8%",
+            "right": "8%",
+            "top": "15%",
+            "bottom": "15%",
+            "containLabel": True
+        },
+        "xAxis": {
+            "type": "category",
+            "boundaryGap": False,
+            "data": meses
+        },
+        "yAxis": [
+            {
+                "type": "value",
+                "name": "Valor CMV Orçado (R$)",
+                "position": "left",
+                "axisLine": {"lineStyle": {"color": "#1E3A8A"}},
+                "axisLabel": {"formatter": "R${value}"}
+            },
+            {
+                "type": "value",
+                "name": "% CMV Orçado",
+                "position": "right",
+                "axisLine": {"lineStyle": {"color": "#22C55E"}},
+                "axisLabel": {"formatter": "{value}%"}
+            }
+        ],
+        "series": [
+            {
+                "name": "Valor CMV Orçado (R$)",
+                "type": "line",
+                "yAxisIndex": 0,
+                "data": dados_cmv_orcado_em_reais,
+                "color": "#1E3A8A"
+            },
+            {
+                "name": "CMV Orçado (%)",
+                "type": "line",
+                "yAxisIndex": 1,
+                "data": dados_cmv_orcado_porcentagem,
+                "color": "#22C55E"
+            }
+        ]
+    }
+
+    st_echarts(options=option, height="400px", key="grafico_cmv_orcado")
+
 
 def main():
     # Sidebar
@@ -30,7 +108,24 @@ def main():
     with col3:
         if st.button('Logout', key='logout'):
             logout()
+
+    df_cmv_orcado = GET_CMV_ORCADO_AB()
+
     st.divider()
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        lista_retirar_casas = ['Bar Léo - Vila Madalena', 'Blue Note SP (Novo)', 'Edificio Rolim', 'Todas as Casas']
+        id_casa, casa, id_zigpay = input_selecao_casas(lista_retirar_casas, 'selecao_casa')
+    df_cmv_orcado = df_cmv_orcado[df_cmv_orcado['Casa'] == casa]
+    with col2:
+        ano = seletor_ano(2024, int(get_last_year(today=datetime.datetime.now()) + 1), 'ano', label='Selecione o ano')
+    df_cmv_orcado = df_cmv_orcado[df_cmv_orcado['Ano'] == ano]
+    st.divider()
+
+    #st.dataframe(df_cmv_orcado, use_container_width=True)
+    grafico_cmv_orcado(df_cmv_orcado)
+    
+    
 
 
 
